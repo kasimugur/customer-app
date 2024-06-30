96c7f3a3f3575b80da57c0c9617ea6dd7011adc7
kasimugur <kasiugur7@gmail.com>
2024-06-30 11:17:43 +0300

    customer

--- src/components/CustomerForm.jsx
+++ src/components/CustomerForm.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import { ContextPage } from "../context"
-
+import { FaCirclePlus } from "react-icons/fa6";
 export default function CustomerForm() {
   const { setCustomerName,setCustomers, customerName, customers } = useContext(ContextPage)
   const addNewItem = (newCustomers) => {
@@ -23,11 +23,11 @@ export default function CustomerForm() {
 
   return (
     <>
-      <form onSubmit={handleSubmit} >
+      <form className="customer-form" onSubmit={handleSubmit} >
         <h2>customer manage system</h2>
         <input o type="text" value={customerName}
           onChange={(e) => setCustomerName(e.target.value)} placeholder="add a new customer" />
-        <button >add</button>
+        <button ><FaCirclePlus /></button>
       </form>
     </>
   )
